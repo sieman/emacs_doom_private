ec26452026e621112b00eae4527d393756c6c5fa
Siegmar Mantei <github@smantei.net>
2020-06-22 06:50:36 +0200

    Move configuraton

--- +modifier.el
+++ +modifier.el
@@ -2,6 +2,13 @@
 ;; Auf Mac Tastaturen ist die Command-Taste über die alles gesteuert
 ;; wird. Daher mag ich es diese für die Control-Taste zu verwenden.
 
+;; =C      = Shorthand for the ctrl-key
+;; =M      = Shorthand for the meta-key 
+;; =S      = Shorthand for the shift-key
+;; =H      = Shorthand for the fn-key
+;; =A      = Shorthand for the alt-key
+;; =s      = Shorthand for the super-key
+
 (when (eq system-type 'darwin)
        (setq
              mac-function-modifier 'meta
