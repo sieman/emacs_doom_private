f31c3fd30dbbe9d8c37153b5b03dc26dccc67e41
Siegmar Mantei <github@smantei.net>
2020-11-16 16:50:15 +0100

    Collering

--- +themes.el
+++ +themes.el
@@ -8,9 +8,10 @@
 (setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'regular))
 
 (custom-set-faces
-             '(show-paren-match ((t (:background "red" :foreground "blue" :inverse-video t :weight bold))))
-             '(region ((t (:foreground "#f1c40f" :background "#2c3e50" :inverse-video t))))
-             `(cursor ((t (:foreground "white" :background "red")))))
+ '(show-paren-match ((t (:background "black" :foreground "yellow" :inverse-video t :weight bold))))
+ '(region ((t (:foreground "#f1c40f" :background "#2c3e50" :inverse-video t))))
+ `(cursor ((t (:foreground "white" :background "red")))))
+(custom-set-variables '(cursor-type 'bar))
 ;; All themes are safe to load
 (setq custom-safe-themes t)
 
@@ -34,32 +35,32 @@
 
 ;; Color the border of windows according to the `highlight' color of the doom's theme
 (add-hook! 'doom-load-theme-hook
-  ;; A more visible window border
-  (set-face-attribute 'vertical-border nil :foreground (doom-color 'highlight))
-  ;; Flycheck errors use the color of functions
-  (after! flycheck
-      (set-face-attribute 'flycheck-error nil
-                          :underline `(:color ,(doom-color 'functions)
-                                       :style wave)))
-  ;; Custom doom-one configuration
-  (when (custom-theme-enabled-p 'doom-one)
-    (after! org
-      ;; Purple boxes for Org BEGIN_SRC and END_SRC
-      (set-face-attribute 'org-block-begin-line nil
-                          :background "#5c3d5c"
-                          :foreground "#a16ba1"
-                          :weight 'bold
-                          :height 0.9
-                          :box '(:line-width 2 :color "#5c3d5c")))))
+           ;; A more visible window border
+           (set-face-attribute 'vertical-border nil :foreground (doom-color 'highlight))
+           ;; Flycheck errors use the color of functions
+           (after! flycheck
+             (set-face-attribute 'flycheck-error nil
+                                 :underline `(:color ,(doom-color 'functions)
+                                              :style wave)))
+           ;; Custom doom-one configuration
+           (when (custom-theme-enabled-p 'doom-one)
+             (after! org
+               ;; Purple boxes for Org BEGIN_SRC and END_SRC
+               (set-face-attribute 'org-block-begin-line nil
+                                   :background "#5c3d5c"
+                                   :foreground "#a16ba1"
+                                   :weight 'bold
+                                   :height 0.9
+                                   :box '(:line-width 2 :color "#5c3d5c")))))
 
 (add-hook! org-mode
-  ;; Document title font
-  (set-face-attribute 'org-document-title nil :height 2.0)
-  ;; Face of keyword DONE (Green like strings)
-  ;; (set-face-attribute 'org-done nil :foreground "#98be65")
-  ;; Face of keyword TODO or [ ] (Purple like keywords)
-  ;; (set-face-attribute 'org-todo nil :foreground "#c678dd")
-  ;; Face of ellipsis symbol (Purple like keywords)
-  (set-face-attribute 'org-ellipsis nil :foreground "#c678dd")
-  ;; Face of the entire headline of a DONE line
-  (set-face-attribute 'org-headline-done nil :foreground nil))
+           ;; Document title font
+           (set-face-attribute 'org-document-title nil :height 2.0)
+           ;; Face of keyword DONE (Green like strings)
+           ;; (set-face-attribute 'org-done nil :foreground "#98be65")
+           ;; Face of keyword TODO or [ ] (Purple like keywords)
+           ;; (set-face-attribute 'org-todo nil :foreground "#c678dd")
+           ;; Face of ellipsis symbol (Purple like keywords)
+           (set-face-attribute 'org-ellipsis nil :foreground "#c678dd")
+           ;; Face of the entire headline of a DONE line
+           (set-face-attribute 'org-headline-done nil :foreground nil))
