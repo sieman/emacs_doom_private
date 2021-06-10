ce69793da767fb9a530f0acb2be8ff4f3d1cb2af
Siegmar Mantei <github@smantei.net>
2021-06-10 07:32:44 +0200

    P -> p

---
+++ packages.el
@@ -0,0 +1,17 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; [[file:../.e/sieman.doom.d/config.org::*Package loading][Package loading:2]]
+(package! move-text)
+(package! multiple-cursors)
+(package! beacon)
+(package! rg)
+(package! rainbow-mode)
+(package! sublimity)
+(package! xah-fly-keys)
+(package! four-modifier :recipe (:host github :repo "sieman/four-modifier"))
+(package! neo-layout-fly-keys :recipe (:host github :repo "sieman/neo-layout-fly-keys"))
+(package! elegant-emacs :recipe (:host github :repo "rougier/elegant-emacs"))
+(package! meow)
+(package! aggressive-indent-mode)
+;; Package loading:2 ends here
