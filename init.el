1d0af71d0cb3e909f65f3d8a03ef4309c7b0f9a0
Siegmar Mantei <github@smantei.net>
2025-12-03 17:54:40 +0100

    :-)

--- init.el
+++ init.el
@@ -7,71 +7,76 @@
 
 
  :ui
- doom              ; what makes DOOM look the way it does
- doom-dashboard    ; a nifty splash screen for Emacs
- hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
- ligatures         ; ligatures and symbols to make your code pretty again
- modeline          ; snazzy, Atom-inspired modeline, plus API
- nav-flash         ; blink cursor line after big motions
- ophints           ; highlight the region an operation acts on
+ doom                     ; what makes DOOM look the way it does
+ doom-dashboard           ; a nifty splash screen for Emacs
+ hl-todo                  ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+ ligatures                ; ligatures and symbols to make your code pretty again
+ modeline                 ; snazzy, Atom-inspired modeline, plus API
+ nav-flash                ; blink cursor line after big motions
+ ophints                  ; highlight the region an operation acts on
  emoji
- (popup +defaults)   ; tame sudden yet inevitable temporary windows
- unicode           ; extended unicode support for various languages
- vc-gutter         ; vcs diff in the fringe
- vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+ (popup +defaults)              ; tame sudden yet inevitable temporary windows
+ unicode                        ; extended unicode support for various languages
+ vc-gutter                      ; vcs diff in the fringe
+ vi-tilde-fringe                ; fringe tildes to mark beyond EOB
  (window-select +switch-window) ; visually switch windows
- zen               ; distraction-free coding or writing
- tabs              ; Zentauer
+ zen                            ; distraction-free coding or writing
+ tabs                           ; Zentauer
  smooth-scroll
  deft
 
  :editor
- file-templates    ; auto-snippets for empty files
- fold              ; (nigh) universal code folding
- multiple-cursors  ; editing in many places at once
- snippets          ; my elves. They type so I don't have to
+ file-templates                         ; auto-snippets for empty files
+ fold                                   ; (nigh) universal code folding
+ multiple-cursors                       ; editing in many places at once
+ snippets                               ; my elves. They type so I don't have to
  format
-
+ lispy
+ 
  :emacs
- (dired +icons +dirvish)    ; making dired pretty [functional]
- electric          ; smarter, keyword-based electric-indent
- (ibuffer +icons)           ; interactive buffer management
- (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
- vc                ; version-control and Emacs, sitting in a tree
+ (dired +icons +dirvish) ; making dired pretty [functional]
+ electric                ; smarter, keyword-based electric-indent
+ (ibuffer +icons)        ; interactive buffer management
+ (undo +tree)            ; persistent, smarter undo for your inevitable mistakes
+ vc                      ; version-control and Emacs, sitting in a tree
 
  :term
- eshell            ; the elisp shell that works everywhere
+ eshell                                 ; the elisp shell that works everywhere
 
  :checkers
- syntax            ; tasing you for every semicolon you forget
- (spell +hunspell +flyspell) ; tasing you for misspelling mispelling
+ syntax                              ; tasing you for every semicolon you forget
+ (spell +hunspell +flyspell)         ; tasing you for misspelling mispelling
 
  :tools
  debugger
  tree-sitter
  (lookup +dictionary)
- editorconfig      ; let someone else argue about tabs vs spaces
+ editorconfig                      ; let someone else argue about tabs vs spaces
  pdf
- (eval +overlay)     ; run code, run (also, repls)
- (lookup +dictionary +docsets) ; navigate your code and its documentation
- (lsp +peek)                   ; for latex
- magit             ; a git porcelain for Emacs
+ (eval +overlay)                      ; run code, run (also, repls)
+ (lookup +dictionary +docsets)        ; navigate your code and its documentation
+ (lsp +peek)                          ; for latex
+ magit                                ; a git porcelain for Emacs
+ llm
+ 
 
  :os
- (:if IS-MAC macos)  ; improve compatibility with macOS
+ (:if IS-MAC macos)                     ; improve compatibility with macOS
 
  :lang
- (clojure +tree-sitter)           ; java with a lisp
- data              ; config/data formats
- emacs-lisp        ; drown in parentheses
- (json +tree-sitter)              ; At least it ain't XML
- (javascript +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
- (latex +lsp)             ; writing papers in Emacs has never been so fun
- (org +dragndrop +roam2 +journal)               ; organize your plain life in plain text
- (sh               ; she sells {ba,z,fi}sh shells on the C xor
-   +tree-sitter)
- (web +tree-sitter)               ; the tubes
+ (clojure +tree-sitter)          ; java with a lisp
+ data                            ; config/data formats
+ emacs-lisp                      ; drown in parentheses
+ (json +tree-sitter)             ; At least it ain't XML
+ (javascript +tree-sitter)       ; all(hope(abandon(ye(who(enter(here))))))
+ (latex +lsp)                    ; writing papers in Emacs has never been so fun
+ (org +dragndrop +roam +journal) ; organize your plain life in plain text
+ (sh                             ; she sells {ba,z,fi}sh shells on the C xor
+  +tree-sitter)
+ (web +tree-sitter)                     ; the tubes
  (java +tree-sitter)
+ (markdown +tree-sitter +grip)
+ 
  :config
  literate
  (default +bindings +emacs +emacs-bindings)
